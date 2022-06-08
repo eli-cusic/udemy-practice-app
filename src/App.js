0752761b17e30b6a2c8fbde848f8a37c0909e654
Jasmine Cain <jasmine.cain@kindful.com>
2022-06-08 10:18:24 -0500

    adding Jas components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Eli from './components/eli_components/Eli';
 import BradyFactory from './components/brady_components/BradyFactory';
 import Laura from './components/laura_components/Laura'
+import Jasmine from './components/jasmine_components/Jasmine'
 
 function App() {
   return (
